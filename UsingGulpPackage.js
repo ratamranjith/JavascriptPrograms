3c9692dbaa73d3738efffb2e98979f952f5c68d0
RANJITH KUMAR VENKATACHALAM <ratamranjith@gmail.com>
2016-11-13 22:08:52 +0530

    Add files via upload

---
+++ UsingGulpPackage.js
@@ -0,0 +1,15 @@
+/* Book-Name: Learning JavaScript
+   Book-Author-Name: Ethan Brown
+   Scripter-Name: Ranjith Kumar Venkatachalam
+   Script-Name: UsingGulpPackage.js
+   - Gulp Automates the Repetitive tasks
+   - Gulp Should be installed on each system Globally [npm install -g gulp]
+   - For Local dev dependancy [npm install --save-dev gulp]
+*/
+
+const gulp = require('gulp');
+task = gulp.task('default', function(){
+
+console.log("hello World");
+})
+
