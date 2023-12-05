5c284bfa0389ad18d990794a5fff06de99a21b01
RANJITH KUMAR VENKATACHALAM <ratamranjith@gmail.com>
2023-12-05 16:49:26 +0530

    Problem-1: Swapping of numbers

---
+++ JSProblems/1/script.js
@@ -0,0 +1,14 @@
+function swaps(){
+var a = document.getElementsByName("var1")[0].value;
+var b = document.getElementsByName("var2")[0].value;
+
+//-----------------------------------------------------
+// Usage of mathematical operations to swap the numbers
+a = (Number(a) + Number(b));
+b = (Number(a) - Number(b));
+a = (Number(a) - Number(b));
+console.log(a);
+console.log(b);
+document.getElementsByName('var3')[0].value = a;
+document.getElementsByName('var4')[0].value = b;
+}
