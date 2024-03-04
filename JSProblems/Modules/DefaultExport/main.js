e1e1080b909935c12ba673aebae7f0fdac2b9314
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-03-05 00:36:54 +0530

    Default Export

--- JSProblems/Modules/DefaultExport/main.js
+++ JSProblems/Modules/DefaultExport/main.js
@@ -1,6 +1,9 @@
 import {names} from './DefaultExport.js'
 
 // Since the user provided as array. We need to iterate using array methods
+// console.log(namesData);
 names.forEach(element => {
-    console.log(element);
+    let namesData = document.getElementsByClassName("names");
+    namesData.innerHTML += `   ${element}`;
+    console.log(namesData.innerHTML);
 });
