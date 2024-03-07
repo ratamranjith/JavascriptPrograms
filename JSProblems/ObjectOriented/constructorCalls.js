b33011b51733370b3fac82a2b033d6bff21f55e2
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-03-07 23:22:17 +0530

    default binding and constructor calls

---
+++ JSProblems/ObjectOriented/constructorCalls.js
@@ -0,0 +1,12 @@
+/*
+Constructor Calls:
+    1. Create a brand new empty object
+    2. *Link that object to another object
+    3. Call function with this set to the new object
+    4. If function doesn't return an object, assume return of this
+*/
+function dataSample(){
+    console.log("Hello there");
+}
+
+let ConstructorCall = new dataSample();
