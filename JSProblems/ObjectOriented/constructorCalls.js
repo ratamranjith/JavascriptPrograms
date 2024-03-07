4ec7880996ad9eea6e80c664a372d0456bd91fc9
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-03-07 23:25:22 +0530

    minor update

--- JSProblems/ObjectOriented/constructorCalls.js
+++ JSProblems/ObjectOriented/constructorCalls.js
@@ -2,7 +2,7 @@
 Constructor Calls:
     1. Create a brand new empty object
     2. *Link that object to another object
-    3. Call function with this set to the new object
+    3. Call function with this set to the new object (call or apply)
     4. If function doesn't return an object, assume return of this
 */
 function dataSample(){
