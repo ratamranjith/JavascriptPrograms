673d985e55768ae63c087d54929b66a17e0d9c2b
INTRUDER <dragula145@gmail.com>
2024-08-24 22:56:33 +0530

    removing all files from the repository

--- JSProblems/2/script.js
+++
@@ -1,6 +0,0 @@
-function counter() {
-    const textInput = document.querySelector('.textInput');
-    const counterSpan = document.querySelector('.counter');
-    const count = textInput.value.length;
-    counterSpan.textContent = `${count} ${count === 1 ? 'character' : 'characters'}`;
-}
